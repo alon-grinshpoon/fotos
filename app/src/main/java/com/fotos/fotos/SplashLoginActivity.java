3fa2e8d7247ae8ccdc30a2bf146177b3030f1b05
Alon Grinshpoon <alon.grinshpoon@yahoo.com>
2015-09-03 14:18:47 +0300

    Splash Screen Update

--- app/src/main/java/com/fotos/fotos/SplashLoginActivity.java
+++ app/src/main/java/com/fotos/fotos/SplashLoginActivity.java
@@ -4,16 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashLoginActivity extends Activity {
 
     /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.splashlogin);
 
         /* New Handler to start the Menu-Activity

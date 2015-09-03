2e259586dd1665616b7d7452b50d3f107129cea3
yoada <atzmoni@gmail.com>
2015-09-03 17:26:16 +0300

    Add FB Login and move to main activity

--- app/src/main/java/com/fotos/fotos/SplashLoginActivity.java
+++ app/src/main/java/com/fotos/fotos/SplashLoginActivity.java
@@ -2,24 +2,35 @@ package com.fotos.fotos;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
 
 import java.util.Arrays;
 
 public class SplashLoginActivity extends Activity {
 
+    private CallbackManager callbackManager;
+
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private static final String TAG = "FBLogin";
 
     /** Called when the activity is first created. */
     @Override
@@ -28,6 +39,15 @@ public class SplashLoginActivity extends Activity {
 
         // Init FB SDK, must do this before anything else
         FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+
+        final AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken newAccessToken) {
+                updateWithToken(newAccessToken);
+            }
+        };
 
         // remove title
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -38,5 +58,67 @@ public class SplashLoginActivity extends Activity {
 
         LoginButton authButton = (LoginButton) findViewById(R.id.authButton);
         authButton.setReadPermissions(Arrays.asList("user_photos", "user_friends"));
+
+        authButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                // Save the access token
+                AccessToken.setCurrentAccessToken(loginResult.getAccessToken());
+                Log.d(TAG, "Button Success");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "Button Cancel");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.e(TAG, "Button Error");
+            }
+        });
+
+
+        updateWithToken(AccessToken.getCurrentAccessToken());
+
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+//        new Handler().postDelayed(new Runnable(){
+//            @Override
+//            public void run() {
+//               /* Create an Intent that will start the Menu-Activity. */
+//               Intent mainIntent = new Intent(SplashLoginActivity.this,MainActivity.class);
+//               SplashLoginActivity.this.startActivity(mainIntent);
+//               SplashLoginActivity.this.finish();
+//           }
+//        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void updateWithToken(AccessToken currentAccessToken) {
+
+        if (currentAccessToken != null) {
+            Log.d(TAG, "signed in");
+
+            new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+               /* Create an Intent that will start the Menu-Activity. */
+               Intent mainIntent = new Intent(SplashLoginActivity.this,MainActivity.class);
+               SplashLoginActivity.this.startActivity(mainIntent);
+               SplashLoginActivity.this.finish();
+           }
+            }, 0);
+        }
+        else {
+            Log.d(TAG, "NOT signed in");
+        }
     }
 }
